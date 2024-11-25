0d7aa2f738bc574c4358202f7d1554050ea7f1f9
liuxukun2000 <xukunliu2025@u.northwestern.edu>
2024-11-25 02:13:08 -0600

    clean up

--- examples/agents/vanilla_agent_with_callbacks.py
+++ examples/agents/vanilla_agent_with_callbacks.py
@@ -26,34 +26,11 @@ warnings.filterwarnings("ignore")
 
 config = AgentConfig.from_file("examples/agents/configs/vanilla_agent.yaml")
 
-# class MyHandler(BaseCallBackHandler):
-#     def on_tool_start(self, source, *args, **kwargs):
-#         print("Tool Start")
-#         print(source)
-#         print(args)
-#         print(kwargs)
-
-#     def on_tool_end(self, source, result):
-#         print("Tool End")
-#         print(source)
-#         print(result)
-
-#     def on_tool_error(self, source, result):
-#         print("Tool Error")
-#         print(source)
-#         print(result)
-# print(config)
-
 agent = VanillaAgent.from_config(config)
 handler = RichOutputHandler()
 agent.tools[0].callback_manager.add(handler)
 agent.model.callback_manager.add(handler)
 agent.callback_manager.add(handler)
-# print(agent.tools[0].callback_manager)
-# print(json.dumps(agent.schema, indent=2))
-agent.run("Search Trump on Wikipedia")
-# print(agent.run("Search Trump on Wikipedia"))
 
-# for i in agent.history:
-#     print(i)
-# print(json.dumps(i, indent=2))
+agent.run("Search Trump on Wikipedia")
+
