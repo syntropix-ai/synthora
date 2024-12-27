c2d8d3037574cfb0d806a6116fd01cdc7401b4c1
liuxukun2000 <xukunliu2025@u.northwestern.edu>
2024-12-26 20:36:10 -0600

    update example

--- examples/toolkits/webpage.py
+++ examples/toolkits/webpage.py
@@ -25,4 +25,8 @@ tool = reader.sync_tools[0]
 
 print(json.dumps(tool.schema, indent=2))
 
-print(tool.run("https://github.com/camel-ai/camel/pull/1315"))
+print(
+    tool.run(
+        "https://github.com/adbar/trafilatura/blob/master/docs/Trafilatura_Overview.ipynb"
+    )
+)
