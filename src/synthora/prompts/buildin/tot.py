6f09d0c8639a0333054152349e8f2bade43be6d5
liuxukun2000 <xukunliu2025@u.northwestern.edu>
2025-01-14 00:28:29 -0600

    reformat code

--- src/synthora/prompts/buildin/tot.py
+++ src/synthora/prompts/buildin/tot.py
@@ -1,7 +1,25 @@
+# LICENSE HEADER MANAGED BY add-license-header
+#
+# Copyright 2024-2025 Syntropix-AI.org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
 ZeroShotTOTProposePrompt = """
 You are an expert problem-solving agent capable of solving problems step by step. Your task is to solve the problem incrementally, executing only **one step at a time** and responding with only one action per step. Each step consists of the following:
 
-1. **Think**: Identify the next logical step in solving the problem. 
+1. **Think**: Identify the next logical step in solving the problem.
     You should carefully describe your thought process and the reasoning behind choosing this step.
     Analyze, break down the current state of the problem and determine the most appropriate action.
     Even if the step seems trivial, you still MUST provide a detailed explanation.
@@ -19,7 +37,7 @@ You CANNOT answer the question directly. Instead, you must guide the user throug
 Your analysis should be as detailed as possible, providing clear explanations and reasoning for each step, even
 if the step seems trivial or obvious.
 Don't forget to verify the final answer!
-"""
+"""  # noqa: E501
 
 ZeroShotTOTEvalPrompt = """
 You are a Scoring Agent designed to evaluate the performance of other Agents based on their response history and user-provided questions. Use the following scoring guidelines:
@@ -36,7 +54,8 @@ The more detailed the Agent's analysis, the better the score.
 If the Agent outputs an answer directly without reasoning or verification, assign 0 points.
 Agents must verify their answers before marking the task as finished (set finished = True). Verification is mandatory.
 Encourage multi-step thinking, problem decomposition, and gradual resolution of the problem with thorough validation.
-Your task is to evaluate the Agent's responses against these guidelines and provide a score. Be objective and strict in your evaluation based on the criteria.
+Your task is to evaluate the Agent's responses against these guidelines and provide a score.
+Be objective and strict in your evaluation based on the criteria.
 
 DO NOT SET THE FINISHED FLAG UNLESS THE AGENT HAS VERIFIED THE FINAL ANSWER!!!
-"""
+"""  # noqa: E501
