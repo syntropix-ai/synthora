d34c5a06234fef07a14fea858f79c1224687f35b
liuxukun2000 <xukunliu2025@u.northwestern.edu>
2025-01-17 21:24:44 -0600

    pass precommit

--- src/synthora/toolkits/weather.py
+++ src/synthora/toolkits/weather.py
@@ -1,28 +1,37 @@
-import json
+# LICENSE HEADER MANAGED BY add-license-header
+#
+# Copyright 2024-2025 Syntropix-AI.org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import os
+from datetime import datetime
 from typing import Literal, Optional
 
-from datetime import datetime
 from synthora.toolkits.base import BaseToolkit
 from synthora.toolkits.decorators import tool
 from synthora.types.enums import Err, Ok, Result
 
 
 class WeatherToolkit(BaseToolkit):
-
     def __init__(self, api_key: Optional[str] = None) -> None:
         super().__init__()
-        try:
-            import pyowm
-        except ImportError:
-            raise ImportError(
-                "Please install `pyowm` first. You can install it by running "
-                "`pip install pyowm`."
-            )
         self.api_key = api_key or os.getenv("OPENWEATHERMAP_API_KEY")
         if not self.api_key:
             raise ValueError(
-                "API key for weatherapi.com not provided. Please set the WEATHER_API_KEY environment variable."
+                "API key for weatherapi.com not provided."
+                + "Please set the WEATHER_API_KEY environment variable."
             )
 
     @tool
@@ -52,12 +61,12 @@ class WeatherToolkit(BaseToolkit):
             units (Literal['meters', 'miles']): Units for visibility
                 distance. (default: :obj:`meters`)
         """
-        import pyowm
-
-        owm = pyowm.OWM(self.api_key)
-        mgr = owm.weather_manager()
-
         try:
+            import pyowm
+
+            owm = pyowm.OWM(self.api_key)
+            mgr = owm.weather_manager()
+
             wind_units = "meters_sec" if units == "meters" else "miles_hour"
             observation = mgr.weather_at_place(city)
             weather = observation.weather
@@ -82,12 +91,17 @@ class WeatherToolkit(BaseToolkit):
                 f"{temperature['feels_like']}°{temp_units.title()}. "
                 f"Max temp: {temperature['temp_max']}°{temp_units.title()}, "
                 f"Min temp: {temperature['temp_min']}°{temp_units.title()}. "
-                f"Wind: {wind_data.get("speed")} {wind_units} at {wind_data.get("deg", "N/A")} degrees. "
+                f"Wind: {wind_data.get("speed")} {wind_units} "
+                f"at {wind_data.get("deg", "N/A")} degrees. "
                 f"Visibility: {visibility} {units}. "
                 f"Sunrise at {sunrise_time}, Sunset at {sunset_time}."
             )
 
             return Ok(weather_report)
-
+        except ImportError:
+            raise ImportError(
+                "Please install `pyowm` first. You can install it by running "
+                "`pip install pyowm`."
+            )
         except Exception as e:
             return Err(e, f"An exception occurred during the search: {e}")
