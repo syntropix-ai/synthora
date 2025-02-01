5618fc2dd4942538521875791bf64fea5421df87
liuxukun2000 <xukunliu2025@u.northwestern.edu>
2025-02-01 17:15:49 -0600

    fix precommit

--- src/synthora/workflows/buildins/condition.py
+++ src/synthora/workflows/buildins/condition.py
@@ -1,12 +1,32 @@
+# LICENSE HEADER MANAGED BY add-license-header
+#
+# Copyright 2024-2025 Syntropix-AI.org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from typing import Any, Callable
+
+from synthora.workflows import task
 from synthora.workflows.base_task import BaseTask
 from synthora.workflows.context.base import BaseContext
-from typing import Any, Callable
-from synthora.workflows import task
 
 
-def if_else(condition_func: Callable[..., bool], name_true: str, name_false: str) -> BaseTask:
+def if_else(
+    condition_func: Callable[..., bool], name_true: str, name_false: str
+) -> BaseTask:
     @task(flat_result=True)
-    def _condition(context: BaseContext, *args: Any, **kwargs: Any) -> None:
+    def _condition(context: BaseContext, *args: Any, **kwargs: Any) -> Any:
         result = condition_func(context, *args, **kwargs)
         if result:
             context.skip(name_false)
@@ -15,5 +35,5 @@ def if_else(condition_func: Callable[..., bool], name_true: str, name_false: str
         if kwargs:
             return *args, kwargs
         return args
-            
-    return _condition
+
+    return _condition  # type: ignore[no-any-return]
