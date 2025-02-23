424aa6c590f73f9893a422a3a09075ccee8e8a96
xukunliu <xukunliu2025@u.northwestern.edu>
2025-02-23 01:05:33 -0600

    add Copyright

--- tests/memories/test_summary_memory.py
+++ tests/memories/test_summary_memory.py
@@ -1,9 +1,25 @@
+# LICENSE HEADER MANAGED BY add-license-header
+#
+# Copyright 2024-2025 Syntropix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import pytest
+
 from synthora.memories.base import BaseMemory
-from synthora.memories.full_context_memory import FullContextMemory
-from synthora.memories.recent_n_memory import RecentNMemory
 from synthora.memories.summary_memory import SummaryMemory
-from synthora.messages import BaseMessage, user
+from synthora.messages import user
 from synthora.models.openai_chat import OpenAIChatBackend
 
 
@@ -19,7 +35,9 @@ class TestSummaryMemory:
         assert isinstance(memory.summary_model, OpenAIChatBackend)
 
     def test_init_with_custom_model(self):
-        memory = SummaryMemory(summary_model=OpenAIChatBackend.default(model_type="gpt-4o-mini"))
+        memory = SummaryMemory(
+            summary_model=OpenAIChatBackend.default(model_type="gpt-4o-mini")
+        )
         assert memory.summary_model.model_type == "gpt-4o-mini"
 
     def test_append(self):
@@ -41,7 +59,7 @@ class TestSummaryMemory:
         memory.append(user("I'm from San Francisco"))
         assert len(memory) == 2
         assert memory[-1].content == "I'm from San Francisco"
-    
+
     async def test_async_summary(self):
         memory = SummaryMemory(n=2, cache_size=2)
         memory.append(user("Hi, My name is John"))
@@ -49,5 +67,3 @@ class TestSummaryMemory:
         memory.append(user("I'm from San Francisco"))
         assert len(memory) == 2
         assert memory[-1].content == "I'm from San Francisco"
-        
-        
